dd2644743b7ea9e21bc58784f051097b2c9ed078
IsaacMagno <isaacmagno98@gmail.com>
2023-10-13 18:20:24 -0300

    feat: add activities rates

---
+++ src/app/(main)/components/Activities.tsx
@@ -0,0 +1,205 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+import userActivitiesImage from "/public/userStats/activities.svg";
+import ratesImage from "/public/userStats/rate.svg";
+
+const Activities = () => {
+  const [showRate, setShowRate] = useState(false);
+
+  return (
+    <>
+      <div className="flex items-center justify-evenly gap-2 my-4 ">
+        <Image
+          src={userActivitiesImage}
+          alt="user stats"
+          className="w-10 h-10"
+        />
+
+        <h1 className="text-xl font-semibold ">Atividades</h1>
+        <Image
+          src={ratesImage}
+          className="w-5 hover:cursor-pointer hover:p-[1px]"
+          onClick={() => setShowRate(!showRate)}
+        />
+      </div>
+      <ul className="list-none space-y-2">
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">Km Corridos</p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">5:1</p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">
+              Saltos de Corda
+            </p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                1800:1
+              </p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">Km Pedalados</p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                20:1
+              </p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">
+              Treino Superior
+            </p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                300:1
+              </p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">
+              Treino Abdominal
+            </p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                500:1
+              </p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">
+              Treino Inferior
+            </p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                300:1
+              </p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">
+              Refeições Saudáveis
+            </p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">8:1</p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">Litros de Água</p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                10:1
+              </p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">Horas de Sono</p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                240:1
+              </p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">
+              Horas Estudando
+            </p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">8:1</p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">
+              Horas Meditando
+            </p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">2:1</p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b">
+            <p className="text-sm lg:text-[15px] font-medium">Horas Lendo</p>
+            {showRate ? (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">3:1</p>
+            ) : (
+              <p className="text-sm lg:text-[15px] font-bold opacity-80">
+                9999
+              </p>
+            )}
+          </span>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Activities;
--- src/app/(main)/components/UserStats.tsx
+++ src/app/(main)/components/UserStats.tsx
@@ -2,148 +2,67 @@ import React from "react";
 import Image from "next/image";
 
 import userStatsImage from "/public/userStats/stats.svg";
-import userActivitiesImage from "/public/userStats/activities.svg";
+import Activities from "./Activities";
 
 const userStats = () => {
   return (
     <section className="w-full h-full mt-4 md:mt-0 lg:min-w-[18rem] max-w-[18rem] md:w-[30vh] p-2 lg:px-4 fixed right-0 top-0 border-l-2">
-      <div className="flex items-center justify-center gap-2">
+      <div className="flex items-center justify-evenly gap-2">
         <Image src={userStatsImage} alt="user stats" className="w-10 h-10" />
 
         <h1 className="text-xl font-semibold my-4 ">Caracteristica</h1>
+        <div></div>
       </div>
 
       <ul className="list-none space-y-2">
         <li>
           <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Força</p>
+            <span className="text-sm lg:text-[15px] font-medium flex gap-2">
+              <p className="w-14">( STR )</p>
+              <p>Força</p>
+            </span>
             <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
           </span>
         </li>
         <li>
           <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Agilidade</p>
+            <span className="text-sm lg:text-[15px] font-medium flex gap-2">
+              <p className="w-14">( DEX )</p>
+              <p>Destreza</p>
+            </span>
             <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
           </span>
         </li>
         <li>
           <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Inteligencia</p>
+            <span className="text-sm lg:text-[15px] font-medium flex gap-2">
+              <p className="w-14">( INT )</p>
+              <p>Inteligência</p>
+            </span>
+            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
+          </span>
+        </li>
+        <li>
+          <span className="flex justify-between border-b ">
+            <span className="text-sm lg:text-[15px] font-medium flex gap-2">
+              <p className="w-14">( CON )</p>
+              <p>Constituição</p>
+            </span>
             <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
           </span>
         </li>
         <li>
           <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Vitalidade</p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Sabedoria</p>
+            <span className="text-sm lg:text-[15px] font-medium flex gap-2">
+              <p className="w-14">( WIS )</p>
+              <p>Sabedoria</p>
+            </span>
             <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
           </span>
         </li>
       </ul>
 
-      <div className="flex items-center justify-center gap-2 my-4 ">
-        <Image
-          src={userActivitiesImage}
-          alt="user stats"
-          className="w-10 h-10"
-        />
-
-        <h1 className="text-xl font-semibold ">Atividades</h1>
-      </div>
-
-      <ul className="list-none space-y-2">
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Km Corridos</p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">
-              Saltos de Corda
-            </p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Km Pedalados</p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">
-              Treino Superior
-            </p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">
-              Treino Abdominal
-            </p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">
-              Treino Inferior
-            </p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">
-              Refeições Saudáveis
-            </p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Litros de Água</p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Horas de Sono</p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">
-              Horas Estudando
-            </p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">
-              Horas Meditando
-            </p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-        <li>
-          <span className="flex justify-between border-b">
-            <p className="text-sm lg:text-[15px] font-medium">Horas Lendo</p>
-            <p className="text-sm lg:text-[15px] font-bold opacity-80">9999</p>
-          </span>
-        </li>
-      </ul>
+      <Activities />
     </section>
   );
 };
