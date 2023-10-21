1eb56a499fe93848d0f5885645b39bd17d6ed3ae
IsaacMagno <isaacmagno98@gmail.com>
2023-10-20 22:05:50 -0300

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import image404 from "/public/404.webp";
+import Image from "next/image";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-zinc-950">
+      <Image src={image404} alt="Not found" className="" />
+      <h1 className="text-3xl font-bold">Página não encontrada</h1>
+      <Link
+        href="/"
+        className="bg-zinc-800 p-3 rounded font-bold mt-12 hover:opacity-70 "
+      >
+        Voltar
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
